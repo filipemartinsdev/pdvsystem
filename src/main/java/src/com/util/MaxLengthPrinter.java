0eb1bfb88e8988c0cbebf6fc66b3c56e8f47a423
filipemartinsdev <dev.filipemartins@gmail.com>
2025-07-14 19:51:44 -0300

    add util package

---
+++ src/main/java/src/com/util/MaxLengthPrinter.java
@@ -0,0 +1,42 @@
+package src.com.util;
+
+public class MaxLengthPrinter {
+    private final int maxLength;
+
+    public MaxLengthPrinter(int max){
+        this.maxLength = max;
+    }
+
+    public void print(String text){
+        char[]textAsCharArr = text.toCharArray();
+        int currentSize = 0;
+
+        while(currentSize < maxLength - 3) {
+            System.out.print(textAsCharArr[currentSize]);
+            currentSize++;
+        }
+
+        System.out.println("...");
+    }
+
+    public String get(String text){
+        char[]textAsCharArr;
+        char[]outAsCharArr;
+
+        if(text.length()>=this.maxLength){
+            textAsCharArr = text.toCharArray();
+            outAsCharArr = new char[text.length()-(text.length()-this.maxLength)-3];
+        } else {
+            return text;
+//            outAsCharArr = new char[text.length()-(this.maxLength-text.length())-3];
+        }
+
+        int currentSize = 0;
+
+        while(currentSize < maxLength - 3) {
+            outAsCharArr[currentSize] = textAsCharArr[currentSize];
+            currentSize++;
+        }
+        return String.copyValueOf(outAsCharArr)+"...";
+    }
+}
